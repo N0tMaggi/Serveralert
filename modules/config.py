5d51bd4c8cc8940e5045756e56a928cea61ae7d7
Maggi <admin@maggi.dev>
2026-01-31 17:45:04 +0100

    Update db eval error

--- modules/config.py
+++ modules/config.py
@@ -64,118 +64,119 @@ def env_int(key, default):
     return env_value(key, default, cast=int)
 
 
-DEFAULT_WEBHOOK_FALLBACK = env_value(
-    "WEBHOOK_DEFAULT",
-    "https://discord.com/api/webhooks/1431187987282133113/3MLfgswtruHYCTgDO3Q2E3t84wKJtOVz6WW2dG2SmhuiNre3dRjKbiNy_VwSAXpWFS6t"
-)
+def build_default_config():
+    default_webhook_fallback = env_value(
+        "WEBHOOK_DEFAULT",
+        "https://discord.com/api/webhooks/1431187987282133113/3MLfgswtruHYCTgDO3Q2E3t84wKJtOVz6WW2dG2SmhuiNre3dRjKbiNy_VwSAXpWFS6t"
+    )
 
-DEFAULT_CONFIG = {
-    "discord_webhook_url": DEFAULT_WEBHOOK_FALLBACK,
-    "thresholds": {
-        "cpu": env_float("CPU_THRESHOLD", 50),
-        "memory": env_float("MEMORY_THRESHOLD", 50),
-        "disk": env_float("DISK_THRESHOLD", 80),
-        "load_average": env_float("LOAD_AVG_THRESHOLD", 5.0),
-        "connections": env_int("CONNECTION_THRESHOLD", 1000),
-        "processes": env_int("PROCESS_THRESHOLD", 500)
-    },
-    "check_interval_seconds": env_int("CHECK_INTERVAL_SECONDS", 300),
-    "monitored_services": env_list(
-        "MONITORED_SERVICES",
-        [
-            "apache2",
-            "mariadb",
-            "ssh",
-            "maggi.dev.web.service",
-            "codeserv.cloud.web.service",
-            "isafemboy.gay.web.service"
-        ]
-    ),
-    "monitored_directories": env_list(
-        "MONITORED_DIRECTORIES",
-        [
-            "/etc",
-            "/usr/bin",
-            "/usr/sbin",
-            "/bin",
-            "/sbin",
-            "/var/www"
-        ]
-    ),
-    "alert_min_interval_seconds": env_int("ALERT_MIN_INTERVAL", 120),
-    "request_timeout_seconds": env_int("REQUEST_TIMEOUT_SECONDS", 5),
-    "request_max_retries": env_int("REQUEST_MAX_RETRIES", 3),
-    "alert_ping_everyone": env_bool("ALERT_PING_EVERYONE", True),
-    "file_monitor": {
-        "hash_max_file_size_mb": env_int("FILE_HASH_MAX_MB", 5),
-        "hash_chunk_size": env_int("FILE_HASH_CHUNK_SIZE", 65536)
-    },
-    "health_server": {
-        "enabled": env_bool("HEALTH_SERVER_ENABLED", True),
-        "host": env_value("HEALTH_SERVER_HOST", "127.0.0.1"),
-        "port": env_int("HEALTH_SERVER_PORT", 8765)
-    },
-    "log_monitor": {
-        "geoip_lookup": env_bool("GEOIP_LOOKUP_ENABLED", True),
-        "geoip_cache_ttl": env_int("GEOIP_CACHE_TTL", 3600)
-    },
-    "database": {
-        "host": env_value("DB_HOST", "127.0.0.1"),
-        "port": env_int("DB_PORT", 3306),
-        "user": env_value("DB_USER", "root"),
-        "password": env_value("DB_PASS", ""),
-        "backup_interval_minutes": env_int("DB_BACKUP_INTERVAL_MINUTES", 30)
-    },
-    "webhooks": {
-        "default": DEFAULT_WEBHOOK_FALLBACK,
-        "db": env_value("WEBHOOK_DB", ""),
-        "resource": env_value("WEBHOOK_RESOURCE", ""),
-        "service_status": env_value("WEBHOOK_SERVICE_STATUS", ""),
-        "service_change": env_value("WEBHOOK_SERVICE_CHANGE", ""),
-        "file_monitor": env_value("WEBHOOK_FILE_MONITOR", ""),
-        "ssh_success": env_value("WEBHOOK_SSH_SUCCESS", ""),
-        "ssh_failed": env_value("WEBHOOK_SSH_FAILED", ""),
-        "sudo": env_value("WEBHOOK_SUDO", ""),
-        "privilege": env_value("WEBHOOK_PRIVILEGE", ""),
-        "password": env_value("WEBHOOK_PASSWORD", ""),
-        "kernel": env_value("WEBHOOK_KERNEL", ""),
-        "firewall": env_value("WEBHOOK_FIREWALL", "")
-    },
-    "embed": {
-        "title_prefix": env_value("EMBED_TITLE_PREFIX", "ServerAlert"),
-        "footer_text": env_value("EMBED_FOOTER_TEXT", "ServerAlert"),
-        "separator": env_value("EMBED_SEPARATOR", "------------------------------"),
-        "thumbnail_url": env_value("EMBED_THUMB_URL", ""),
-        "image_url": env_value("EMBED_IMAGE_URL", ""),
-        "image_warning_url": env_value("EMBED_IMAGE_WARNING_URL", ""),
-        "show_top_processes": env_bool("EMBED_SHOW_TOP_PROCESSES", True)
-    },
-    "detections": {
-        "resources": env_bool("DETECT_RESOURCES", True),
-        "network_load": env_bool("DETECT_NETWORK_LOAD", True),
-        "critical_services": env_bool("DETECT_CRITICAL_SERVICES", True),
-        "service_changes": env_bool("DETECT_SERVICE_CHANGES", True),
-        "log_monitor": env_bool("DETECT_LOG_MONITOR", True),
-        "file_monitor": env_bool("DETECT_FILE_MONITOR", True),
-        "db_backup": env_bool("DETECT_DB_BACKUP", True),
-        "health_server": env_bool("DETECT_HEALTH_SERVER", True),
-        "cpu": env_bool("DETECT_CPU", True),
-        "memory": env_bool("DETECT_MEMORY", True),
-        "disk": env_bool("DETECT_DISK", True),
-        "process_count": env_bool("DETECT_PROCESS_COUNT", True),
-        "connections": env_bool("DETECT_CONNECTIONS", True),
-        "load_average": env_bool("DETECT_LOAD_AVG", True),
-        "service_status": env_bool("DETECT_SERVICE_STATUS", True),
-        "ssh_login": env_bool("DETECT_SSH_LOGIN", True),
-        "ssh_failed": env_bool("DETECT_SSH_FAILED", True),
-        "sudo": env_bool("DETECT_SUDO", True),
-        "privilege": env_bool("DETECT_PRIVILEGE", True),
-        "password_change": env_bool("DETECT_PASSWORD_CHANGE", True),
-        "kernel": env_bool("DETECT_KERNEL", True),
-        "firewall": env_bool("DETECT_FIREWALL", True),
-        "file_changes": env_bool("DETECT_FILE_CHANGES", True)
+    return {
+        "discord_webhook_url": default_webhook_fallback,
+        "thresholds": {
+            "cpu": env_float("CPU_THRESHOLD", 50),
+            "memory": env_float("MEMORY_THRESHOLD", 50),
+            "disk": env_float("DISK_THRESHOLD", 80),
+            "load_average": env_float("LOAD_AVG_THRESHOLD", 5.0),
+            "connections": env_int("CONNECTION_THRESHOLD", 1000),
+            "processes": env_int("PROCESS_THRESHOLD", 500)
+        },
+        "check_interval_seconds": env_int("CHECK_INTERVAL_SECONDS", 300),
+        "monitored_services": env_list(
+            "MONITORED_SERVICES",
+            [
+                "apache2",
+                "mariadb",
+                "ssh",
+                "maggi.dev.web.service",
+                "codeserv.cloud.web.service",
+                "isafemboy.gay.web.service"
+            ]
+        ),
+        "monitored_directories": env_list(
+            "MONITORED_DIRECTORIES",
+            [
+                "/etc",
+                "/usr/bin",
+                "/usr/sbin",
+                "/bin",
+                "/sbin",
+                "/var/www"
+            ]
+        ),
+        "alert_min_interval_seconds": env_int("ALERT_MIN_INTERVAL", 120),
+        "request_timeout_seconds": env_int("REQUEST_TIMEOUT_SECONDS", 5),
+        "request_max_retries": env_int("REQUEST_MAX_RETRIES", 3),
+        "alert_ping_everyone": env_bool("ALERT_PING_EVERYONE", True),
+        "file_monitor": {
+            "hash_max_file_size_mb": env_int("FILE_HASH_MAX_MB", 5),
+            "hash_chunk_size": env_int("FILE_HASH_CHUNK_SIZE", 65536)
+        },
+        "health_server": {
+            "enabled": env_bool("HEALTH_SERVER_ENABLED", True),
+            "host": env_value("HEALTH_SERVER_HOST", "127.0.0.1"),
+            "port": env_int("HEALTH_SERVER_PORT", 8765)
+        },
+        "log_monitor": {
+            "geoip_lookup": env_bool("GEOIP_LOOKUP_ENABLED", True),
+            "geoip_cache_ttl": env_int("GEOIP_CACHE_TTL", 3600)
+        },
+        "database": {
+            "host": env_value("DB_HOST", "127.0.0.1"),
+            "port": env_int("DB_PORT", 3306),
+            "user": env_value("DB_USER", "root"),
+            "password": env_value("DB_PASS", ""),
+            "backup_interval_minutes": env_int("DB_BACKUP_INTERVAL_MINUTES", 30)
+        },
+        "webhooks": {
+            "default": default_webhook_fallback,
+            "db": env_value("WEBHOOK_DB", ""),
+            "resource": env_value("WEBHOOK_RESOURCE", ""),
+            "service_status": env_value("WEBHOOK_SERVICE_STATUS", ""),
+            "service_change": env_value("WEBHOOK_SERVICE_CHANGE", ""),
+            "file_monitor": env_value("WEBHOOK_FILE_MONITOR", ""),
+            "ssh_success": env_value("WEBHOOK_SSH_SUCCESS", ""),
+            "ssh_failed": env_value("WEBHOOK_SSH_FAILED", ""),
+            "sudo": env_value("WEBHOOK_SUDO", ""),
+            "privilege": env_value("WEBHOOK_PRIVILEGE", ""),
+            "password": env_value("WEBHOOK_PASSWORD", ""),
+            "kernel": env_value("WEBHOOK_KERNEL", ""),
+            "firewall": env_value("WEBHOOK_FIREWALL", "")
+        },
+        "embed": {
+            "title_prefix": env_value("EMBED_TITLE_PREFIX", "ServerAlert"),
+            "footer_text": env_value("EMBED_FOOTER_TEXT", "ServerAlert"),
+            "separator": env_value("EMBED_SEPARATOR", "------------------------------"),
+            "thumbnail_url": env_value("EMBED_THUMB_URL", ""),
+            "image_url": env_value("EMBED_IMAGE_URL", ""),
+            "image_warning_url": env_value("EMBED_IMAGE_WARNING_URL", ""),
+            "show_top_processes": env_bool("EMBED_SHOW_TOP_PROCESSES", True)
+        },
+        "detections": {
+            "resources": env_bool("DETECT_RESOURCES", True),
+            "network_load": env_bool("DETECT_NETWORK_LOAD", True),
+            "critical_services": env_bool("DETECT_CRITICAL_SERVICES", True),
+            "service_changes": env_bool("DETECT_SERVICE_CHANGES", True),
+            "log_monitor": env_bool("DETECT_LOG_MONITOR", True),
+            "file_monitor": env_bool("DETECT_FILE_MONITOR", True),
+            "db_backup": env_bool("DETECT_DB_BACKUP", True),
+            "health_server": env_bool("DETECT_HEALTH_SERVER", True),
+            "cpu": env_bool("DETECT_CPU", True),
+            "memory": env_bool("DETECT_MEMORY", True),
+            "disk": env_bool("DETECT_DISK", True),
+            "process_count": env_bool("DETECT_PROCESS_COUNT", True),
+            "connections": env_bool("DETECT_CONNECTIONS", True),
+            "load_average": env_bool("DETECT_LOAD_AVG", True),
+            "service_status": env_bool("DETECT_SERVICE_STATUS", True),
+            "ssh_login": env_bool("DETECT_SSH_LOGIN", True),
+            "ssh_failed": env_bool("DETECT_SSH_FAILED", True),
+            "sudo": env_bool("DETECT_SUDO", True),
+            "privilege": env_bool("DETECT_PRIVILEGE", True),
+            "password_change": env_bool("DETECT_PASSWORD_CHANGE", True),
+            "kernel": env_bool("DETECT_KERNEL", True),
+            "firewall": env_bool("DETECT_FIREWALL", True),
+            "file_changes": env_bool("DETECT_FILE_CHANGES", True)
+        }
     }
-}
 
 CONFIG_PATH = os.environ.get(
     "SERVERALERT_CONFIG",
@@ -195,7 +196,7 @@ def _deep_update(original, updates):
 
 
 def load_config():
-    config = copy.deepcopy(DEFAULT_CONFIG)
+    config = copy.deepcopy(build_default_config())
     if os.path.exists(CONFIG_PATH):
         try:
             with open(CONFIG_PATH, "r", encoding="utf-8") as config_file:
